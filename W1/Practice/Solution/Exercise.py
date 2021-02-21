ae4adfc87f9467b7359c2fb9bc5a20e35fadb56e
nwjun <63900029+nwjun@users.noreply.github.com>
2021-02-21 16:34:30 +0800

    Create Exercise.py

---
+++ W1/Practice/Solution/Exercise.py
@@ -0,0 +1,54 @@
+##########################
+#no1
+a = "6"
+print(int(a)/6)
+# 'a' need to be converted to integer
+# in python, semi-colon is not required
+>>> 1.0
+
+a += " is even"
+print(a)
+>>> "6 is even"
+
+b = a
+print(b)
+# association of '=' is from right to left (value of right is assigned to left)
+>>> 6 is even
+
+c = "hello world"
+print(c)
+# "" is needed for string
+
+##########################
+#no2
+>>> (1) and (4) is valid
+
+##########################
+#no3
+a = (8 / 2) + (5 ** 3) - (10 * 3)
+>>> 99
+
+b = 10 + ((4 % 2) * (1 + 2))
+>>> 10
+
+c = 10 + (2 / 4) - 2
+>>> 8.5
+
+d = (20 * 5) % 2
+>>> 0
+
+e = (3 ** (4 ** 2))
+>>> 43046721
+
+##########################
+#no4
+# Only (3) returns True
+
+##########################
+#no5
+a. <class 'str'>
+b. <class 'int'>
+c. <class 'str'>
+d. <class 'list'>
+e. <class 'tuple'>
+
