ed3edb56d6bc329c056935d2037645262ee60984
nwjun <63900029+nwjun@users.noreply.github.com>
2021-02-21 16:43:14 +0800

    Create p2.py

---
+++ W1/Practice/Solution/Project/p2/p2.py
@@ -0,0 +1,7 @@
+num = input("Enter item number: ")
+price = input("Enter unit price: ")
+date = input("Enter purchase date (mm/dd/yyyy):")
+
+print("Item\t\tUnit\t\tPurchase")
+print("\t\tPrice\t\tDate")
+print(num + "\t\t$ " + price + "\t\t"+ date)
