6ed7432eb730722accc30e43828bb2f8de11d11b
nwjun <63900029+nwjun@users.noreply.github.com>
2021-02-23 23:21:29 +0800

    Update p3.py
    Add comment

--- W1/Practice/Solution/Project/p3/p3.py
+++ W1/Practice/Solution/Project/p3/p3.py
@@ -1,2 +1,5 @@
 amount = float(input("Enter an amount: "))
+# convert string to float to do mathematical operation
+
 print("With tax added: $" + str(amount* 1.05))
+# convert floar to string to concatenate with another string
